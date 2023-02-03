4c90a4b6b949e072d9ffb186666c94e1daf97cc1
ilCary <carisiodigiampietro54@gmail.com>
2023-02-03 23:21:52 +0100

    progetto inizializzato

---
+++ GestioneCinema_Be/src/test/java/com/CarisioDiGiampietro/GestioneCinema/GestioneCinema/GestioneCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CarisioDiGiampietro.GestioneCinema.GestioneCinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
